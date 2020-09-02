3840b773afc788f1e1de6ea82bff5822f5d7f758
Patrick5455 <patrickojunde@gmail.com>
2020-09-02 09:01:00 +0100

    trainign various models

--- analysis/interactive/pipeline.py
+++ analysis/interactive/pipeline.py
@@ -1,3 +1,4 @@
 #%%writefile ../scripts/project_package/model_package/pipeline.py 
-
-"pipeline"
+from sklearn.pipeline import FeatureUnion, Pipeline  
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import Pipeline as ImbPipeline, make_pipeline
--- analysis/scripts/project_package/model_package/pipeline.py
+++ analysis/scripts/project_package/model_package/pipeline.py
@@ -1,2 +1,3 @@
-
-"pipeline"
+from sklearn.pipeline import FeatureUnion, Pipeline  
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import Pipeline as ImbPipeline, make_pipeline
